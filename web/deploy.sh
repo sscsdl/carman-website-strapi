c85392f9098872f328ffdb86f2e4fe03fe3f9468
sscsdl <n_nzzzz@hotmail.com>
2026-02-13 11:53:25 +0800

    shell

--- web/deploy.sh
+++ web/deploy.sh
@@ -1,3 +1,4 @@
 export NUXT_PUBLIC_STRAPI_URL=http://118.31.72.147:1337
-#nohup npm run start > log.log 2>&1 &
+#npm run build
+#npm run generate
 npm run deploy
--- web/stop.sh
+++
@@ -1,1 +0,0 @@
-ps aux|grep "carman-website-strapi/web"|grep -v grep|awk '{print $2}'|xargs kill -9
